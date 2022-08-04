62f202dbd0ef6b38bff6852ab051cd9c27c62c79
Marcus Garcia <marcus.a.garcia@icloud.com>
2022-08-03 22:42:14 -0400

    added if else logic to playRound function

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 function getComputerChoice() {
-  const gameArray = ["Rock", "Paper", "Sissors"];
+  const gameArray = ["rock", "paper", "sissors"];
   const computerChoice = gameArray[~~(Math.random() * gameArray.length)];
   return computerChoice;
 }
 
-// function getPlayerChoice() {
-//   const playerSelection = prompt("Choose wisely!");
-//   return playerSelection;
-// }
+function getPlayerChoice() {
+  const playerSelection = prompt("Choose wisely!");
+  return playerSelection;
+}
 
-// const playerSelection = getPlayerChoice();
-// const computerSelection = getComputerChoice();
+const playerSelectionPrompt = getPlayerChoice();
+const playerSelection = playerSelectionPrompt.toLowerCase();
+const computerSelection = getComputerChoice();
 
 function playRound(playerSelection, computerSelection) {
-  console.log(
-    "You lose! " + `${computerSelection}` + " beats " + `${playerSelection}`
-  );
+  if (playerSelection == "rock" && computerSelection == "sissors") {
+    console.log(
+      "You win! " + `${playerSelection}` + " beats " + `${computerSelection}`
+    );
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    console.log(
+      "You win! " + `${playerSelection}` + " beats " + `${computerSelection}`
+    );
+  } else if (playerSelection == "sissors" && computerSelection == "paper") {
+    console.log(
+      "You win! " + `${playerSelection}` + " beats " + `${computerSelection}`
+    );
+  } else if (playerSelection === computerSelection) {
+    console.log(
+      "It's a tie! " +
+        `${playerSelection}` +
+        " cancells out " +
+        `${computerSelection}`
+    );
+  } else {
+    console.log(
+      console.log(
+        "You lose. " +
+          `${playerSelection}` +
+          " loses to " +
+          `${computerSelection}`
+      )
+    );
+  }
 }
 
 playRound(playerSelection, computerSelection);
